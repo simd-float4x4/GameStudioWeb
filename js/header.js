9128a48465738ba918eae46a887ffcd96ade526e
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-03-04 14:04:08 +0900

    送信するボタンの実装

--- js/header.js
+++ js/header.js
@@ -1,3 +1,10 @@
+init();
+
+function init(){
+    var current = sessionStorage.getItem('current');   
+    myFunction(current);
+}
+
 function myFunction(elemName) {
     var x = document.getElementById(elemName);
     var array = ["top", "about", "product", "contact"];
@@ -5,6 +12,7 @@ function myFunction(elemName) {
     for (i = 0; i < array.length; i++) {
         if (elemName === array[i]) {
             x.classList.remove('hidden');
+            sessionStorage.setItem('current', elemName);
         } else {
             var y = document.getElementById(array[i]);
             y.classList.add('hidden');
