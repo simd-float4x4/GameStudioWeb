f0177009b55404a3da2fb39cb081e011690bdfbb
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-03-05 14:31:40 +0900

    法人選択時に会社名を出現させる処理の追加

--- js/form.js
+++ js/form.js
@@ -1,11 +1,16 @@
 function selectUserTypeRadioButton(elemName) {
     var array = ["form-usertype-personal", "form-usertype-official", "form-usertype-other"];
     var x = document.getElementById(elemName);
-
+    var z = document.getElementById("form-company-name");
     for (i = 0; i < array.length; i++) {
         if (elemName === array[i]) {
             x.classList.remove('select-user-type-button');
             x.classList.add('select-user-type-button-activated');
+            if (elemName == array[1]) {
+                z.classList.remove('hidden');
+            } else {
+                z.classList.add('hidden');
+            }
         } else {
             var y = document.getElementById(array[i]);
             y.classList.remove('select-user-type-button-activated');
