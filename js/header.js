b9ca022254e6bbe38b2553b2694e855d60e07c48
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-03-06 21:46:38 +0900

    TODO: カレント表示のマークアップ（下準備）

---
+++ js/header.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".header .wrapper nav ul li a").on("click",  function() {
+        console.log('detected');
+        if( $("[href^='#']") ){
+            $("header-item").removeClass('header-selected');
+            $(this).parent().addClass('header-selected');
+            return false;
+        }
+     });
+});
+   
