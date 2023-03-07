08f231eae227436283dab43cb87d59ad70a47de4
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-03-07 22:57:15 +0900

    仮完成

--- js/header.js
+++ js/header.js
@@ -11,3 +11,10 @@
 //      });
 // });
    
+const headerNavLink = document.querySelectorAll('.header-link');
+
+headerNavLink.forEach((targetLink) => {
+  if (targetLink.href === location.href) {
+    targetLink.parentElement.classList.add('header-selected');
+  }
+});
