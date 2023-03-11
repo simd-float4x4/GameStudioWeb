796a3d5199f33dfe6ba034c264725c2386ee4e4f
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-03-11 13:19:25 +0900

    CONTACT* ドロップダウンリストcss修正/css&js全体的にフォーマット対応

--- js/header.js
+++ js/header.js
@@ -1,12 +1,11 @@
-
 init();
 
 function init() {
     const headerNavLink = document.querySelectorAll('.header-link');
-    
+
     headerNavLink.forEach((targetLink) => {
-      if (targetLink.href === location.href) {
-        targetLink.parentElement.classList.add('header-selected');
-      }
+        if (targetLink.href === location.href) {
+            targetLink.parentElement.classList.add('header-selected');
+        }
     });
 }
